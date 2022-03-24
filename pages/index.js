7a5d541bf71706c225ccb25f9f36c2f0565443e8
mnahapet <mariam_nahapetyan@yahoo.com>
2022-03-24 12:40:30 +0100

    isr

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,7 @@ export default function Home({ posts }) {
 export async function getStaticProps() {
   const posts = (await getPosts()) || [];
   return {
-    props: { posts }
+    props: { posts },
+    revalidate: 1
   };
 }
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -51,9 +51,8 @@ export default PostDetails;
 export async function getStaticProps({ params }) {
   const data = await getPostDetails(params.slug);
   return {
-    props: {
-      post: data
-    }
+    props: { post: data },
+    revalidate: 1
   };
 }
 
